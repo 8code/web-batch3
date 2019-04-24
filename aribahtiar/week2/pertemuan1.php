e5a6145ea25748f14278efed44bedf66f48b5efa
aribahtiarpro <aribahtiarpro@gmail.com>
2019-04-24 17:50:58 +0700

    pundamental bahasa pemrograman

---
+++ aribahtiar/week2/pertemuan1.php
@@ -0,0 +1,81 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar Bahasa Pemrograman</title>
+</head>
+<body>
+
+<?php
+// Ini Mengunakan PHP
+// 1. Variable,
+$nama = (string)"ari bahtiar";
+// echo "dengan PHP : " . $nama;
+// 2. data type,
+$angka = (int)2;
+// var_dump($angka);
+$boolean = true; // false
+$array = array(); // []
+
+// 3. operator, 
+$tambah  = 1 + 2;
+$kali  = 1 * 2;
+// 4. condition, 
+    // if else
+    // if($angka == 1){
+    //     echo "angka 1";
+    // }else if($angka == 2){
+    //     echo "angka 2";
+    // }
+    // else{
+    //     echo "bukan angka 1";
+    // }
+    // case of
+// 5. looping, 
+
+$data = array("data 1","data 2","data 3");
+
+// for($i = 0; $i <= 2; $i++){
+//     echo $data[$i];
+// }
+
+// while($angka <= 5){
+//     echo $angka;
+//     $angka++;
+// }
+
+// foreach($data as $d){
+//     echo $d;
+// }
+
+// 6. array, 
+
+// 7. object, 
+// 8. function, 
+// 9. sorting
+// 10. searching
+?>
+<!-- <div id="data">
+</div> -->
+<script>
+// Ini Menggunakan Javascript
+// 1. Variable (var,let,const)
+var nama = "Ari Bahtiar";
+console.log(nama);
+// 2. data type,
+// 3. operator, 
+// 4. condition, 
+// 5. looping, 
+// 6. array, 
+// 7. object, 
+// 8. function, 
+// 9. sorting
+// 10. searching
+</script>
+
+</body>
+</html>
