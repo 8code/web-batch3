fa9dfa268cec47ae3f59f9f7814eb3b5bfb9a7e0
dewiwss <dewiws27@gmail.com>
2019-05-01 18:54:33 +0700

    update laravelku, buat artikel komentar

--- dewiws/laravel_dewi/resources/views/layouts/app.blade.php
+++ dewiws/laravel_dewi/resources/views/layouts/app.blade.php
@@ -33,6 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                         <li class="nav-item">
+                                <a class="nav-link" href="{{ route('artikel') }}">{{ __('Artikel') }}</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="komentar">Komentar</a>
+                        </li>
 
                     </ul>
 
@@ -41,7 +47,7 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('artikel') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
--- dewiws/laravel_dewi/routes/web.php
+++ dewiws/laravel_dewi/routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/artikel', 'ArtikelController@index')->name('artikel');
+
+Route::get('/komentar', 'KomentarController@index')->name('komentar');
+
+
