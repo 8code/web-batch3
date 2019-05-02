9216ac7fd9c88c468ceb066a584071c22d150823
aribahtiarpro <aribahtiarpro@gmail.com>
2019-05-02 21:11:20 +0700

    laravel CRUD

---
+++ aribahtiar/yucoding-blog/resources/views/artikel.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Artikel</div>
+                @include('artikel.tambah')
+                @include('artikel.edit')
+                @include('artikel.delete')
+                <div class="card-body">
+
+                    <table class="table">
+                        <tr>
+                            <th>Judul</th>
+                            <th>Kategori</th>
+                            <th>Action</th>
+                        </tr>
+                        @foreach ($dataartikel as $artikel)
+                        <tr>
+                            <td> {{ $artikel->judul }}</td>
+                            <td> {{ $artikel->kategori }}</td>
+                            <td> 
+                                <button 
+                                    class="btn btn-primary btn-sm"
+                                    onclick="editartikel({{ $artikel }})"
+                                >
+                                    Edit
+                                </button>
+                                <button 
+                                    class="btn btn-danger btn-sm"
+                                    onclick="deleteartikel({{ $artikel }})"
+                                    >
+                                    Delete
+                                </button>
+
+                                <script>
+                                    function editartikel(artikel){
+
+                                        $("#edit-id").val(artikel.id)
+                                        $("#edit-judul").val(artikel.judul)
+                                        $("#edit-kategori").val(artikel.kategori)
+                                        $("#edit-isi").val(artikel.isi)
+
+                                        $("#modal-editartikel").modal("show")
+                                        
+                                    }
+                                    function deleteartikel(artikel){
+
+                                        // Tambahkan ID
+                                        $("#delete-id").val(artikel.id)
+
+                                        $("#modal-deleteartikel").modal("show");
+
+                                    }
+                                </script>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
